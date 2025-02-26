4dc64805576a54af255cfe5e7a191a2ee31d80e9
CoolCoolTomato <2983315455@qq.com>
2025-02-26 13:12:20 +0800

    默认白色主题全屏

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -194,7 +194,7 @@ function Screen() {
         />
         <WindowContent>
           <Routes>
-            <Route path={Path.Home} element={<Chat />} />
+            <Route path={Path.Home} element={<NewChat />} />
             <Route path={Path.NewChat} element={<NewChat />} />
             <Route path={Path.Masks} element={<MaskPage />} />
             <Route path={Path.Plugins} element={<PluginPage />} />
